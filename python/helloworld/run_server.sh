bce6ecab646b2b61d97bac63ec883b99cee62829
geekan <geekan@foxmail.com>
2015-08-24 17:57:45 +0000

    Fix path to grpc python virtualenv.

--- python/helloworld/run_server.sh
+++ python/helloworld/run_server.sh
@@ -5,5 +5,5 @@
 # ADJUST THIS PATH TO WHERE YOUR ACTUAL LOCATION IS
 GRPC_ROOT=~/github/grpc
 
-$GRPC_ROOT/python2.7_virtual_environment/bin/python greeter_server.py
+$GRPC_ROOT/python_virtual_environment/bin/python greeter_server.py
 
