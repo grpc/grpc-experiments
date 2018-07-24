84b84c2a5b755b88d947f8ff241d3922e629fbd9
Spencer Fang <spencerfang@google.com>
2018-07-24 16:03:15 -0700

    move flags to pkg level, avoid http mux

--- grpc-zpages/docker/static-assets/static_assets_server.go
+++ grpc-zpages/docker/static-assets/static_assets_server.go
@@ -9,13 +9,13 @@ import (
 	"strings"
 )
 
+var port = flag.Int("port", 8080, "local port")
+var wwwRoot = flag.String("wwwroot", "/www", "root for http files")
+
 func main() {
-	var port = flag.Int("port", 8080, "local port")
-	var wwwRoot = flag.String("wwwroot", "/www", "root for http files")
 	flag.Parse()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Got request %s\n", r.URL.Path)
 		localPath := path.Join(*wwwRoot, r.URL.Path[1:])
 		if strings.HasSuffix(localPath, ".css") || strings.HasSuffix(localPath, ".js") {
@@ -26,5 +26,5 @@ func main() {
 	})
 	fmt.Printf("Starting static assets server on port %d . Note this is not the envoy port!\n"+
 		"You should not need to look at this port unless you know what you're doing.\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
