5259aa5493269a44c269e3d94419718603fb39b7
Spencer Fang <spencerfang@google.com>
2018-10-03 08:18:52 -0700

    gdebug: fix codegen script
    Fix bug where we fail to check out correct SHA if dir already exists.
    
    Remove `exec` because it makes the `cp` fail to run.

--- gdebug/buildscripts/channelz_codgen.sh
+++ gdebug/buildscripts/channelz_codgen.sh
@@ -17,9 +17,9 @@ if [[ ! -d "$GITHUB_DIR"/grpc-web ]]; then
   mkdir -p "$GITHUB_DIR"
   cd "$GITHUB_DIR"
   git clone https://github.com/grpc/grpc-web.git
-  cd grpc-web/
-  git checkout "$GRPC_WEB_SHA"
 fi
+cd "$GITHUB_DIR"/grpc-web/
+git checkout "$GRPC_WEB_SHA"
 
 cd "$GITHUB_DIR"/grpc-web//net/grpc/gateway/docker
 docker build -t channelz_grpc_web_prereqs ./prereqs/
@@ -27,6 +27,6 @@ docker build -t channelz_grpc_web_prereqs ./prereqs/
 cd "$GRPC_ZPAGES_DIR"/docker
 docker build -t channelz_codegen codegen
 
-exec docker run --rm -v "$GRPC_ZPAGES_DIR"/docker/codegen/shared_dir:/shared_dir/ channelz_codegen bash -c "function fixFiles() { chown -R $(id -u):$(id -g) /shared_dir; }; trap fixFiles EXIT; /shared_dir/scripts/gen_channelz_pb.sh $GRPC_PROTO_SHA"
+docker run --rm -v "$GRPC_ZPAGES_DIR"/docker/codegen/shared_dir:/shared_dir/ channelz_codegen bash -c "function fixFiles() { chown -R $(id -u):$(id -g) /shared_dir; }; trap fixFiles EXIT; /shared_dir/scripts/gen_channelz_pb.sh $GRPC_PROTO_SHA"
 
 mv "$GRPC_ZPAGES_DIR"/docker/codegen/shared_dir/gen_out/channelz.js "$GRPC_ZPAGES_DIR"/web/channelzui/src/generated/channelz.js
